e7cc93f9b971d21a4eb899a99f4d68c8c838e734
heartfume <gekgees@gmail.com>
2017-09-25 13:52:36 +0530

    Added title and name.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var formmatedName = HTMLheaderName.replace("%data%","Satya Singh");
+ $("#header").prepend(formmatedName);
+
+var formmatedRole = HTMLheaderRole.replace("%data%","Team Lead");
+$("#header").prepend(formmatedRole);
